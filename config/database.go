519651108bac32fe0f09ec0f0d338019fa7d98eb
violasptntels <violaseptiantiana23@gmail.com>
2025-05-16 18:43:20 +0700

    connect database

--- config/database.go
+++ config/database.go
@@ -1,30 +1,49 @@
 package config
 
 import (
-    "context"
-    "log"
-    "os"
-    "time"
+	"context"
+	"log"
+	"os"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var DB *mongo.Database
 
 func ConnectDB() {
-    client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	// Get Mongo URI
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Fatal("MONGO_URI environment variable not set or empty")
+	}
 
-    err = client.Connect(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    DB = client.Database("workwise")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Gunakan DB dari env juga
+	dbName := os.Getenv("MONGO_DB")
+	if dbName == "" {
+		log.Fatal("MONGO_DB environment variable not set or empty")
+	}
+
+	DB = client.Database(dbName)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.5
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.17.3
 )
 
